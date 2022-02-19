c0b5325ba02ec2731f2e7ec500bb31aa0f986212
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-02-19 19:01:55 +0530

    updated

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const bodyParser = require('body-parser')
+const server = jsonServer.create();
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000; // needed when you deploy into heroku.
 
+server.use(bodyParser.json());
 server.use(jsonServer.bodyParser);
+server.use(bodyParser.urlencoded({ extended: true }));
 
-server.use(middlewares)
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
 server.use(router);
 // router.render = (req, res) => {
-//   res.jsonp({
+//   debugger;
+//   res.json({
 //     body: res.locals.data
-//   })
+//   });
 // }
 
 server.listen(port, () => {
